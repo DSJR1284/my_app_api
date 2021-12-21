d5dc698d14ac47c12c2eb73c10938a5bda23bbf4
Doug Jones <DSJR1284@GMAIL.COM>
2021-12-21 10:03:08 -0500

    created seed data for app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Doug = Board.create(title: "Who Am I")
+
+# Post.create(image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvELImLpXH6bMAfLRLInWAa1zsT3Pt2lWIuA&usqp=CAU", description: "I was born at Down State Medical and raise in Crown Heights Brooklyn, NY", board_id: 1)
+
+Post.create(image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkNMNM6DJQkc6n35EHWBmceFymZIBuKi36NQ&usqp=CAU", description: "I am currently a student at Flatiron, hoping to break into the tech industry", board_id: 1)
+
+Post.create(image: "https://scontent-bos3-1.xx.fbcdn.net/v/t1.6435-9/c80.0.206.206a/p206x206/30226718_10156130310659933_4296439288666521600_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=da31f3&_nc_ohc=khJt47VlBLAAX_IonRO&_nc_ht=scontent-bos3-1.xx&oh=00_AT_ihsTkIxXg_9zyYMH6IlPF46g20UiJoxmccuZ_bME6mQ&oe=61E8AA04", description: "I recently became a father of my 1st son to go with my 3 daughters", board_id: 1)
+
+
